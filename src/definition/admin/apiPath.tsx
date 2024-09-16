97379404f5c58d25a78f213571a399996b72e7ab
taewoongjung <taewj@howser.co.kr>
2024-09-17 08:48:16 +0900

    api path 수정

--- src/definition/admin/apiPath.tsx
+++ src/definition/admin/apiPath.tsx
@@ -1,18 +1,18 @@
 // prod
-// export const loginAPI = `https://hoppang.store/api/login`;
-// export const callMeData = `https://hoppang.store/api/me?isAdminPage=true`;
+export const loginAPI = `https://hoppang.store/api/login`;
+export const callMeData = `https://hoppang.store/api/me?isAdminPage=true`;
 
-// export const findAllChassisPriceByCompanyTypeAndChassisType = `https://hoppang.store/api/chassis/prices`;
-// export const findAllAdditionalChassisPriceCriteria = `https://hoppang.store/api/chassis/prices/additions/criteria`;
-// export const reviseAdditionalChassisPriceCriteria = `https://hoppang.store/api/chassis/prices/additions/criteria`;
+export const findAllChassisPriceByCompanyTypeAndChassisType = `https://hoppang.store/api/chassis/prices`;
+export const findAllAdditionalChassisPriceCriteria = `https://hoppang.store/api/chassis/prices/additions/criteria`;
+export const reviseAdditionalChassisPriceCriteria = `https://hoppang.store/api/chassis/prices/additions/criteria`;
 
 
 
 
 // dev
-export const loginAPI = `http://localhost:7070/api/login`;
-export const callMeData = `http://localhost:7070/api/me?isAdminPage=true`;
+// export const loginAPI = `http://localhost:7070/api/login`;
+// export const callMeData = `http://localhost:7070/api/me?isAdminPage=true`;
 //
-export const findAllChassisPriceByCompanyTypeAndChassisType = `http://localhost:7070/api/chassis/prices`;
-export const findAllAdditionalChassisPriceCriteria = `http://localhost:7070/api/chassis/prices/additions/criteria`;
-export const reviseAdditionalChassisPriceCriteria = `http://localhost:7070/api/chassis/prices/additions/criteria`;
+// export const findAllChassisPriceByCompanyTypeAndChassisType = `http://localhost:7070/api/chassis/prices`;
+// export const findAllAdditionalChassisPriceCriteria = `http://localhost:7070/api/chassis/prices/additions/criteria`;
+// export const reviseAdditionalChassisPriceCriteria = `http://localhost:7070/api/chassis/prices/additions/criteria`;
