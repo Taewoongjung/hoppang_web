5f4d7ff8b6e35c77ea86e06a4428cffa766a466c
taewoongjung <taewj@howser.co.kr>
2024-09-18 16:47:03 +0900

    샤시 타입 순서 변경

--- src/definition/chassisType.tsx
+++ src/definition/chassisType.tsx
@@ -1,9 +1,9 @@
 const chassisTypeOptions = [
     { value: 'BalconySingle', label: '발코니단창' },
-    { value: 'InteriorSingle', label: '내창단창' },
-    { value: 'LivingRoomSliding', label: '거실분합창' },
     { value: 'BalconyDouble', label: '발코니이중창' },
+    { value: 'InteriorSingle', label: '내창단창' },
     { value: 'InteriorDouble', label: '내창이중창'},
+    { value: 'LivingRoomSliding', label: '거실분합창' },
 ]
 
 export default chassisTypeOptions;
