627edf3c5379765303e0913fa88a0c31ab535b4b
taewoongjung <taewj@howser.co.kr>
2024-09-30 22:05:02 +0900

    인트로 화면 수정

--- src/pages/calculateChassis/InitialScreen/index.tsx
+++ src/pages/calculateChassis/InitialScreen/index.tsx
@@ -27,41 +27,18 @@ const InitialScreen = (props: {secondStep:boolean, companyType:string, setCompan
                     <tbody>
                         <tr>
                             <td colSpan={2}>
-                                <div style={{
-                                    position: 'fixed',
-                                    top: '50%',
-                                    left: '50%',
-                                    transform: 'translate(-50%, -50%)',
-                                    width: '80%', // 또는 원하는 너비
-                                    maxHeight: '20vh', // 뷰포트 높이의 80%
-                                    overflow: 'auto',
-                                    textAlign: 'center',
-                                    padding: '20px',
-                                    marginTop: 30,
-                                    boxSizing: 'border-box' }}>
-                                    <div style={{fontFamily: 'Cochin', color: 'grey', textAlign: 'center'}}>
-                                        안녕하세요 <strong>호빵</strong>입니다. <br/><br/>
-                                        소비자들이 적정 가격에 샤시를 구매할 수 있도록 지표를 제시합니다.
-                                        <br/><br/>
-                                        호구가 빵명이 되는 그날까지.
-                                    </div>
+                                <div style={{fontFamily: 'Cochin', color: 'grey', width: 300}}>
+                                    안녕하세요 <strong style={{ color: '#444444'}}>호빵</strong>입니다. <br/><br/>
+                                    소비자들이 적정 가격에 샤시를 구매할 수 있도록 지표를 제시합니다.
+                                    <br/><br/>
+                                    <strong style={{ color: '#444444'}}>호</strong>구가 <strong style={{ color: '#444444'}}>빵</strong>명이 되는 그날까지.
                                 </div>
                             </td>
                         </tr>
                         <tr>
                             <td colSpan={2}>
-                                <div style={{
-                                    position: 'fixed',
-                                    overflow: 'auto',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    transform: 'translate(-50%, -50%)',
-                                    width: '60%',
-                                    marginTop: 50
-                                }}>
-                                    <div style={{marginTop: 10}}>
-                                        <Button onClick={handleGetStarted} size={"small"}> 견적 시작하기</Button>
-                                    </div>
+                                <div style={{marginTop: 10}}>
+                                    <Button onClick={handleGetStarted} size={"small"}> 견적 시작하기</Button>
                                 </div>
                             </td>
                         </tr>
