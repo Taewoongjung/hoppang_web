4ce2b72c2ec3fae7a637d36bd631c3912493269e
taewoongjung <taewj@howser.co.kr>
2024-10-03 21:27:43 +0900

    로그인 페이지 base code 추가

--- src/layouts/App/App.tsx
+++ src/layouts/App/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import loadable from "@loadable/component";
 import { Switch, Route, Redirect } from 'react-router-dom';
 
+const LoginPage = loadable(() => import("../../pages/calculateChassis/Login"))
 const CalculationScreen = loadable(() => import('../../pages/calculateChassis/CalculationScreen'));
 
 // 어드민
-const LoginPage = loadable(() => import('../../pages/admin/Login'));
+const AdminLoginPage = loadable(() => import('../../pages/admin/Login'));
 const ChassisPriceDatabaseMainScreen = loadable(() => import('../../pages/admin/ChassisPriceDatabaseMainScreen'));
 const EstimatedDatabaseMainScreen = loadable(() => import('../../pages/admin/EstimatedDatabaseMainScreen'));
 
@@ -13,10 +14,11 @@ const App = () => {
     return (
         <Switch>
             <Redirect exact path="/" to="/chassis/calculator" />
+            <Route path="/login" component={LoginPage} />
             <Route path="/chassis/calculator" component={CalculationScreen} />
 
             {/* ADMIN */}
-            <Route path="/admin/login" component={LoginPage} />
+            <Route path="/admin/login" component={AdminLoginPage} />
             <Route path="/admin/essentials/info" component={ChassisPriceDatabaseMainScreen} />
             <Route path="/admin/essentials/estimates/info" component={EstimatedDatabaseMainScreen} />
         </Switch>
