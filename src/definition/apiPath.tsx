b2ba7890eaabd127dc857a0d2136522fcfb635ae
taewoongjung <taewj@howser.co.kr>
2024-10-05 22:52:45 +0900

    카카오 로그인 api 요청 추가

--- src/definition/apiPath.tsx
+++ src/definition/apiPath.tsx
@@ -1,6 +1,8 @@
 // prod
 export const calculateChassisCall = `https://hoppang.store/api/chassis/calculations/prices`;
+export const kakaoLogin = `https://hoppang.store/api/kakao/auth`;
 
 
 // dev
 // export const calculateChassisCall = `http://localhost:7070/api/chassis/calculations/prices`;
+// export const kakaoLogin = `http://localhost:7070/api/kakao/auth`;
--- src/pages/calculateChassis/Login/index.tsx
+++ src/pages/calculateChassis/Login/index.tsx
@@ -1,28 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../styles.css';
 import axios from "axios";
+import {LeftOutlined} from "@ant-design/icons";
+import {kakaoLogin} from "../../../definition/apiPath";
 
-const API_URL = process.env.REACT_APP_HOPPANG_APP_REQUEST_API_URL;
-const KAKAO_REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
 
 const Login = () => {
 
-    const REST_API_KEY = "da09bfccea21381b988c8ede053a85c5";
-    // const REDIRECT_URI = "http://localhost:7070/api/kakao/signup";
-    const REDIRECT_URI = "http://localhost:3000/chassis/calculator";
+    const [kakaoFirstLoginReqUrl, setKakaoFirstLoginReqUrl] = useState('');
 
     const handleKakaoLogin = () => {
-        // window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+        const callLogin = async () => {
+            axios.post(kakaoLogin, {}, {withCredentials: true})
+                .then((res) => {
+                    setKakaoFirstLoginReqUrl(res.data);
+                })
+                .catch((err) => {
+                    console.log("카카오 로그인 에러 = ", err);
+                })
+        }
+
+        callLogin();
     };
 
+    useEffect(() => {
+        if (kakaoFirstLoginReqUrl) {
+            window.location.href = kakaoFirstLoginReqUrl;
+        }
+    }, [kakaoFirstLoginReqUrl]);
+
     const handleAppleLogin = () => {
-        console.log("애플 로그인 = ", API_URL);
-        console.log("key = ", KAKAO_REST_API_KEY);
+        console.log("애플 로그인 ");
     };
+
 
     return (
         <div className="login-container" style={styles.container}>
             <div className="login-box" style={styles.box}>
+
+                {/*뒤로가기*/}
+                <LeftOutlined
+                    onClick={() => {window.location.href = "/"}}
+                    style={{marginRight: '100%', fontSize: '30px', marginBottom: '50px', color: 'blue'}}
+                />
 
                 <h2 style={styles.subtitle}>호빵 - 호구빵명 프로젝트</h2>
                 {/* 회사 로고 */}
@@ -39,7 +59,7 @@ const Login = () => {
                         onClick={handleKakaoLogin}
                         style={{
                             ...styles.button,
-                            background: `url("/assets/kakao_login_medium_wide.png") no-repeat center center`,
+                            background: `url("/assets/kakao_login_large_wide.png") no-repeat center center`,
                             backgroundSize: 'cover'
                         }}
                     />
@@ -65,51 +85,43 @@ const styles: { [key: string]: React.CSSProperties } = {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: '#fff',
+        width: '100%',
         height: '100vh',
-        backgroundColor: '#f5f5f5',
     },
     box: {
-        backgroundColor: '#fff',
-        padding: '40px',
         borderRadius: '10px',
         boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
         textAlign: 'center',
         width: '100%',
-        maxWidth: '400px',
-    },
-    title: {
-        fontSize: '28px',
-        marginBottom: '10px',
-        color: '#333',
-        fontWeight: 'bold',
+        maxWidth: '600px',
+        padding: '400px',
     },
     logo: {
-        width: '100px', // 로고 크기 조정
+        width: '230px',
         marginBottom: '50px',
     },
     subtitle: {
-        fontSize: '16px',
+        fontSize: '26px',
         color: '#666',
-        // marginBottom: '50px',
     },
     buttonContainer: {
         display: 'flex',
-        alignItems: 'center', justifyContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
         flexDirection: 'column',
         gap: '15px',
     },
     button: {
-        width: '300px',
-        height: '45px',
+        width: '600px',
+        height: '65px',
         border: 'none',
         cursor: 'pointer',
         padding: 0,
         overflow: 'hidden',
         borderRadius: '8px',
-        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
         transition: 'transform 0.2s ease, box-shadow 0.2s ease',
     },
 };
-
 
 export default Login;
