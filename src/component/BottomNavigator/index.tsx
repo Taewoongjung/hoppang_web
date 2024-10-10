3c8396c6b80589964de6b708488d74dce2f1e3a8
taewoongjung <taewj@howser.co.kr>
2024-10-11 01:48:24 +0900

    하단 네비바 클릭 효과 추가 + 필요없는 코드 제거

--- src/component/BottomNavigator/index.tsx
+++ src/component/BottomNavigator/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import './styles.css';
+import {useLocation} from "react-router-dom";
 
 const BottomNavigator = () => {
+
+    const location = useLocation();
 
     return (
         <>
             <nav>
-                <a href="#"><span className="nav-icon"><img src="/assets/BottomNav/home.png" alt="Home"/></span>홈</a>
-                <a href="/chassis/calculator"><span className="nav-icon"><img src="/assets/BottomNav/chassis-icon-3.png" alt="ChassisCal"/></span>샤시견적</a>
-                <a href="#"><span className="nav-icon"><img src="/assets/BottomNav/community.png" alt="Community"/></span>커뮤니티</a>
-                <a href="/login"><span className="nav-icon"><img src="/assets/BottomNav/my-info.png" alt="MyInfo"/></span>마이</a>
+                <a href="#" className={location.pathname === '/home' ? 'active' : ''}><span className="nav-icon"><img src="/assets/BottomNav/home.png" alt="Home"/></span>홈</a>
+                <a href="/chassis/calculator" className={location.pathname === '/chassis/calculator' ? 'active' : ''}><span className="nav-icon"><img src="/assets/BottomNav/chassis-icon-3.png" alt="ChassisCal"/></span>샤시견적</a>
+                <a href="#" className={location.pathname === '/community' ? 'active' : ''}><span className="nav-icon"><img src="/assets/BottomNav/community.png" alt="Community"/></span>커뮤니티</a>
+                <a href="/login" className={location.pathname === '/mypage' ? 'active' : ''}><span className="nav-icon"><img src="/assets/BottomNav/my-info.png" alt="MyInfo"/></span>마이</a>
             </nav>
         </>
     )
