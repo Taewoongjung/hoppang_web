bfd5b47cbb97b325110b8ef48b646280dbb9f806
taewoongjung <taewj@howser.co.kr>
2024-10-22 22:42:35 +0900

    애플 소셜 로그인 path 수정

--- src/definition/apiPath.tsx
+++ src/definition/apiPath.tsx
@@ -7,7 +7,7 @@ export const callVerifyPhoneNumber = `https://hoppang.store/api/phones/validatio
 export const callMeData = `https://hoppang.store/api/me`;
 export const callFinalSocialSignUp = `https://hoppang.store/api/social/users`;
 export const appleLogin = `https://hoppang.store/api/apple/auth`;
-export const appleAuth = `https://hoppang.store/api/apple/signup`;
+export const appleAuth = `https://hoppang.store/api/apple/signup/`;
 
 // dev
 // export const calculateChassisCall = `http://localhost:7070/api/chassis/calculations/prices`;
@@ -18,4 +18,4 @@ export const appleAuth = `https://hoppang.store/api/apple/signup`;
 // export const callMeData = `http://localhost:7070/api/me`;
 // export const callFinalSocialSignUp = `http://localhost:7070/api/social/users`;
 // export const appleLogin = `http://localhost:7070/api/apple/auth`;
-// export const appleAuth = `http://localhost:7070/api/apple/signup`;
+// export const appleAuth = `http://localhost:7070/api/apple/signup/`;
