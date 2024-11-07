def25af04777133ce2d6476c24d22d2fc650832f
taewoongjung <taewj@howser.co.kr>
2024-11-07 13:43:56 +0900

    사용 안하는 기능 주석 처리 - 2

--- src/component/BottomNavigator/index.tsx
+++ src/component/BottomNavigator/index.tsx
@@ -9,7 +9,7 @@ const BottomNavigator = () => {
     return (
         <>
             <nav>
-                <a href="#" className={location.pathname === '/home' ? 'active' : ''}><span className="nav-icon"><img src="/assets/BottomNav/home.png" alt="Home"/></span>홈</a>
+                {/*<a href="#" className={location.pathname === '/home' ? 'active' : ''}><span className="nav-icon"><img src="/assets/BottomNav/home.png" alt="Home"/></span>홈</a>*/}
                 <a href="/chassis/calculator" className={location.pathname === '/chassis/calculator' ? 'active' : ''}><span className="nav-icon"><img src="/assets/BottomNav/chassis-icon-3.png" alt="ChassisCal"/></span>샤시견적</a>
                 {/*<a href="#" className={location.pathname === '/community' ? 'active' : ''}><span className="nav-icon"><img src="/assets/BottomNav/community.png" alt="Community"/></span>커뮤니티</a>*/}
                 <a href="/mypage" className={location.pathname === '/mypage' ? 'active' : ''}><span className="nav-icon"><img src="/assets/BottomNav/my-info.png" alt="MyInfo"/></span>마이</a>
