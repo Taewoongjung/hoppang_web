4de1b108d1f68e77914dcd568714d08416bd127b
taewoongjung <taewj@howser.co.kr>
2024-11-12 18:01:17 +0900

    로딩 화면 리펙토링

--- src/pages/calculateChassis/MyPage/ConfigPage/index.tsx
+++ src/pages/calculateChassis/MyPage/ConfigPage/index.tsx
@@ -93,87 +93,88 @@ const ConfigPage = () => {
         <>
             { loading && <LoadingPage/> }
 
-
-            <div className="container">
-                <div className="box">
-                    <div style={{marginTop: 40}}>
-                        <section className="settings-section">
-                            <header className="settings-header">
-                                <button className="back-button" onClick={() => {window.location.href = '/mypage';}}><LeftOutlined /></button>
-                                <h2>설정</h2>
-                            </header>
-                            <ul className="settings-list">
-                                <li className="settings-item" onClick={showEngPolicyModal}>
-                                    <span>개인정보 처리방침</span>
-                                    <span className="arrow"><RightOutlined /></span>
-                                </li>
-                                {urlParams.get("isLoggedIn") === 'false' &&
-                                    <li className="settings-item" onClick={() => {window.location.href = '/login';}}>
-                                        <span>회원가입</span>
+            { !loading &&
+                <div className="container">
+                    <div className="box">
+                        <div style={{marginTop: 40}}>
+                            <section className="settings-section">
+                                <header className="settings-header">
+                                    <button className="back-button" onClick={() => {window.location.href = '/mypage';}}><LeftOutlined /></button>
+                                    <h2>설정</h2>
+                                </header>
+                                <ul className="settings-list">
+                                    <li className="settings-item" onClick={showEngPolicyModal}>
+                                        <span>개인정보 처리방침</span>
                                         <span className="arrow"><RightOutlined /></span>
                                     </li>
-                                }
-                                {urlParams.get("isLoggedIn") === 'true' &&
-                                    <li className="settings-item" onClick={() => {window.location.href = '/mypage/config/app';}}>
-                                        <span>앱 관리</span>
-                                        <span className="arrow"><RightOutlined /></span>
-                                    </li>
-                                }
-                                {urlParams.get("isLoggedIn") === 'true' &&
-                                    <>
-                                        <li className="settings-item" onClick={() => setWithdrawUserModal(true)}>
-                                            <span>회원탈퇴</span>
+                                    {urlParams.get("isLoggedIn") === 'false' &&
+                                        <li className="settings-item" onClick={() => {window.location.href = '/login';}}>
+                                            <span>회원가입</span>
                                             <span className="arrow"><RightOutlined /></span>
                                         </li>
-                                        <li className="settings-item" onClick={handleLogOut}>
-                                            <span>로그아웃</span>
+                                    }
+                                    {urlParams.get("isLoggedIn") === 'true' &&
+                                        <li className="settings-item" onClick={() => {window.location.href = '/mypage/config/app';}}>
+                                            <span>앱 관리</span>
                                             <span className="arrow"><RightOutlined /></span>
                                         </li>
-                                    </>
-                                }
-                            </ul>
-                        </section>
+                                    }
+                                    {urlParams.get("isLoggedIn") === 'true' &&
+                                        <>
+                                            <li className="settings-item" onClick={() => setWithdrawUserModal(true)}>
+                                                <span>회원탈퇴</span>
+                                                <span className="arrow"><RightOutlined /></span>
+                                            </li>
+                                            <li className="settings-item" onClick={handleLogOut}>
+                                                <span>로그아웃</span>
+                                                <span className="arrow"><RightOutlined /></span>
+                                            </li>
+                                        </>
+                                    }
+                                </ul>
+                            </section>
 
-                        <Modal
-                            title="정말로 탈퇴 하시겠습니까?"
-                            centered
-                            open={withdrawUserModal}
-                            onOk={handleWithdrawUser}
-                            okText={"동의"}
-                            cancelText={"취소"}
-                            onCancel={() => setWithdrawUserModal(false)}
-                        >
-                            <p>회원 탈퇴 후 모든 유저 데이터가 사라지게 됩니다. 그래도 하시겠습니까?</p>
-                        </Modal>
+                            <Modal
+                                title="정말로 탈퇴 하시겠습니까?"
+                                centered
+                                open={withdrawUserModal}
+                                onOk={handleWithdrawUser}
+                                okText={"동의"}
+                                cancelText={"취소"}
+                                onCancel={() => setWithdrawUserModal(false)}
+                            >
+                                <p>회원 탈퇴 후 모든 유저 데이터가 사라지게 됩니다. 그래도 하시겠습니까?</p>
+                            </Modal>
 
 
-                        <Modal title="Basic Modal"
-                               open={isShowEngPolicyModalOpen}
-                               onOk={handleEngPolicyOk}
-                               okText={"확인"}
-                               destroyOnClose={true}
-                        >
-                            <strong>Terms &amp; Conditions</strong><br/>
-                            <p>These terms and conditions apply to the Hoppang app (hereby referred to as "Application") for mobile devices that was created by (hereby referred to as "Service Provider") as a Free service.</p>
-                            <br/>
-                            <p>Upon downloading or utilizing the Application, you are automatically agreeing to the following terms...</p>
-                            <div>
-                                <p>Please note that the Application utilizes third-party services that have their own Terms and Conditions. Below are the links to the Terms and Conditions of the third-party service providers used by the Application:</p>
-                            </div>
-                            <br/>
-                            <p>The Service Provider may wish to update the application at some point...</p>
-                            <br/>
-                            <strong>Changes to These Terms and Conditions</strong>
-                            <p>The Service Provider may periodically update their Terms and Conditions...</p>
-                            <br/>
-                            <strong>Contact Us</strong>
-                            <p>If you have any questions or suggestions about the Terms and Conditions, please do not hesitate to contact the Service Provider at aipooh8882@naver.com.</p>
-                            <hr/>
-                            <p>This Terms and Conditions page was generated by <a href="https://app-privacy-policy-generator.nisrulz.com/" target="_blank" rel="noopener noreferrer">App Privacy Policy Generator</a></p>
-                        </Modal>
+                            <Modal title="Basic Modal"
+                                   open={isShowEngPolicyModalOpen}
+                                   onOk={handleEngPolicyOk}
+                                   okText={"확인"}
+                                   destroyOnClose={true}
+                            >
+                                <strong>Terms &amp; Conditions</strong><br/>
+                                <p>These terms and conditions apply to the Hoppang app (hereby referred to as "Application") for mobile devices that was created by (hereby referred to as "Service Provider") as a Free service.</p>
+                                <br/>
+                                <p>Upon downloading or utilizing the Application, you are automatically agreeing to the following terms...</p>
+                                <div>
+                                    <p>Please note that the Application utilizes third-party services that have their own Terms and Conditions. Below are the links to the Terms and Conditions of the third-party service providers used by the Application:</p>
+                                </div>
+                                <br/>
+                                <p>The Service Provider may wish to update the application at some point...</p>
+                                <br/>
+                                <strong>Changes to These Terms and Conditions</strong>
+                                <p>The Service Provider may periodically update their Terms and Conditions...</p>
+                                <br/>
+                                <strong>Contact Us</strong>
+                                <p>If you have any questions or suggestions about the Terms and Conditions, please do not hesitate to contact the Service Provider at aipooh8882@naver.com.</p>
+                                <hr/>
+                                <p>This Terms and Conditions page was generated by <a href="https://app-privacy-policy-generator.nisrulz.com/" target="_blank" rel="noopener noreferrer">App Privacy Policy Generator</a></p>
+                            </Modal>
+                        </div>
                     </div>
                 </div>
-            </div>
+            }
         </>
     )
 }
