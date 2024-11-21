cf1c5a07db00f75d7c30b9794cada02666ccc061
taewoongjung <taewj@howser.co.kr>
2024-11-22 00:32:35 +0900

    애플 로그인 이미지 변경

--- src/pages/calculateChassis/Login/index.tsx
+++ src/pages/calculateChassis/Login/index.tsx
@@ -73,7 +73,6 @@ const useResponsiveStyles = () => {
 const Login = () => {
 
     const urlParams = new URLSearchParams(window.location.search);
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
     const handleKakaoLogin = () => {
         const callLogin = async () => {
@@ -158,7 +157,7 @@ const Login = () => {
                         onClick={handleAppleLogin}
                         style={{
                             ...styles.button,
-                            background: `url("/assets/appleid_button@2x.png") no-repeat center center`,
+                            background: `url("/assets/appleid_button@4x.png") no-repeat center center`,
                             backgroundSize: 'cover',
                             backgroundColor: 'black',
                             marginTop: "-9px"
