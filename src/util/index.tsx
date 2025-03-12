9d37b1aa59a855cf77864832d39b5bd2fef5a007
taewoongjung <taewj@howser.co.kr>
2025-03-13 01:39:49 +0900

    [랜딩페이지] 유저 동향 방문시간 타임존 서울로 지정 - 4

--- src/util/index.tsx
+++ src/util/index.tsx
@@ -92,7 +92,7 @@ export const convertAdditionalChassisPriceInfoToKo = (target:string) => {
     }
 }
 
-
 export const formatDateTime = (date: Date) => {
-    return date.toString();
+    const pad = (num: number) => num.toString().padStart(2, '0');
+    return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())} ${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}`;
 };
