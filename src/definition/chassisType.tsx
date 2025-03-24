07bd422a8529f49298b455f7e6a14abd3a61ae51
taewoongjung <taewj@howser.co.kr>
2025-03-24 09:07:00 +0900

    [고객] 샤시 견적 화면에 픽스창, 터닝도어 추가

--- src/definition/chassisType.tsx
+++ src/definition/chassisType.tsx
@@ -4,6 +4,8 @@ const chassisTypeOptions = [
     { value: 'InteriorSingle', label: '내창단창' },
     { value: 'InteriorDouble', label: '내창이중창'},
     { value: 'LivingRoomSliding', label: '거실분합창' },
+    { value: 'Fixed', label: '픽스창' },
+    { value: 'Turning', label: '터닝도어' },
 ]
 
 export default chassisTypeOptions;
