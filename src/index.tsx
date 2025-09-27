8412adb264be74f7c25a85efe9fbd8dbe96a27c7
taewoongjung <aipooh8882@naver.com>
2025-09-27 22:43:16 +0900

    미리 빌드 된 파일이 읽히게 entry 파트에서 분기처리 로직 추가

--- src/index.tsx
+++ src/index.tsx
@@ -14,11 +14,14 @@ const app = (
     </BrowserRouter>
 );
 
-// react-snap으로 pre-render된 HTML이 있는지 확인
-if (rootElement && rootElement.hasChildNodes()) {
-    // Pre-rendered HTML이 있으면 hydrate
+// data-reactroot 속성이나 HTML 내용으로 확인
+const isPrerendered =
+    rootElement?.hasAttribute('data-reactroot') ||
+    // @ts-ignore
+    rootElement?.innerHTML.length > 0;
+
+if (rootElement && isPrerendered) {
     hydrateRoot(rootElement, app);
 } else if (rootElement) {
-    // 일반적인 경우 render
     createRoot(rootElement).render(app);
 }
