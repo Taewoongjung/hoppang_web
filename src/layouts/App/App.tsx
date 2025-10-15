e4bfedcd2d2ed48ef4173fa087fcbd0c027615a4
taewoongjung <taewj@howser.co.kr>
2025-10-15 15:47:16 +0900

    V1 파일 제거 및 그에 따른 로직 정리

--- src/layouts/App/App.tsx
+++ src/layouts/App/App.tsx
@@ -2,19 +2,6 @@ import React from 'react';
 import loadable from "@loadable/component";
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-// const CalculationInitialScreen = loadable(() => import('../../pages/hoppangMainStream/InitialScreen'));
-// const CalculationScreen = loadable(() => import('../../pages/hoppangMainStream/CalculationScreen'));
-// const MyPage = loadable(() => import('../../pages/hoppangMainStream/MyPage'));
-// const LoginPage = loadable(() => import("../../pages/hoppangMainStream/Login"));
-// const LoginFirstStepPage = loadable(() => import("../../pages/hoppangMainStream/Login/LoginFirstStep"));
-// const LoginSecondStepPage = loadable(() => import("../../pages/hoppangMainStream/Login/LoginSecondStep"));
-// const MyPageConfigPage  = loadable(() => import('../../pages/hoppangMainStream/MyPage/ConfigPage'));
-// const MyPageAppConfigPage  = loadable(() => import('../../pages/hoppangMainStream/MyPage/ConfigPage/AppConfigPage'));
-// const MyPageEstimationHistoriesPage = loadable(() => import('../../pages/hoppangMainStream/MyPage/EstimationHistory'));
-// const EstimationDetailPage = loadable(() => import('../../pages/hoppangMainStream/MyPage/EstimationDetailPage'));
-// const DuplicatedSsoLoginErrorPage = loadable(() => import('../../pages/hoppangMainStream/Login/DuplicateLoginPage'));
-// const CounselMainScreen = loadable(() => import('../../pages/hoppangMainStream/Counsel'));
-
 // 어드민
 const AdminLoginPage = loadable(() => import('../../pages/admin/Login'));
 const ChassisPriceDatabaseMainScreen = loadable(() => import('../../pages/admin/ChassisPriceDatabaseMainScreen'));
@@ -101,23 +88,6 @@ const App = () => {
 
                     {/* OFFICIAL */}
                     <Route path="/official" component={LandingPage}/>
-
-                    {/* V1 */}
-                    {/*<Route path="/chassis/calculator" component={CalculationInitialScreen}/>*/}
-                    {/*<Route path="/:oauthtype/chassis/calculator" component={CalculationInitialScreen}/>*/}
-                    {/*<Route path="/chassis/estimation/calculator/" component={CalculationScreen}/>*/}
-
-                    {/*<Route path="/mypage/config/app" component={MyPageAppConfigPage}/>*/}
-                    {/*<Route path="/mypage/config" component={MyPageConfigPage}/>*/}
-                    {/*<Route path="/mypage/estimation/histories" component={MyPageEstimationHistoriesPage}/>*/}
-                    {/*<Route path="/mypage/estimation/:estimationId" component={EstimationDetailPage}/>*/}
-                    {/*<Route path="/mypage" component={MyPage}/>*/}
-                    {/*<Route path="/counsel" component={CounselMainScreen}/>*/}
-
-                    {/*<Route path="/login/duplicate" component={DuplicatedSsoLoginErrorPage}/>*/}
-                    {/*<Route path="/login/second" component={LoginSecondStepPage}/>*/}
-                    {/*<Route path="/login/first" component={LoginFirstStepPage}/>*/}
-                    {/*<Route path="/login" component={LoginPage}/>*/}
             </Switch>
         );
 }
