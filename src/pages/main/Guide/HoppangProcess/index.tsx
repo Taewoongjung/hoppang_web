0c428251f084822b4dadec5f8f244f9552e7711b
taewoongjung <aipooh8882@naver.com>
2026-01-20 16:38:56 +0900

    견적 받으러가기로 문구 수정

--- src/pages/main/Guide/HoppangProcess/index.tsx
+++ src/pages/main/Guide/HoppangProcess/index.tsx
@@ -91,7 +91,7 @@ const HoppangProcess = () => {
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
                                           d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>
                                 </svg>
-                                <span className="text-lg">1단계부터 시작! 무료 견적 확인하기</span>
+                                <span className="text-lg">견적 받으러 가기</span>
                                 <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
                                           d="M9 5l7 7-7 7"/>
