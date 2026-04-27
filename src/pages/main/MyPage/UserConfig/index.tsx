a35fe4927ce5509daa594cdcfe9a5c2072006a62
taewoongjung <aipooh8882@naver.com>
2026-04-27 15:57:53 +0900

    마이페이지 앱 버전 표시 갱신
    - 기존 웹 마이페이지의 호빵 앱 버전 표기를 v3.3.4로 변경

--- src/pages/main/MyPage/UserConfig/index.tsx
+++ src/pages/main/MyPage/UserConfig/index.tsx
@@ -261,7 +261,7 @@ const ConfigPage = () => {
                                     <img src="/assets/hoppang-character.png" alt="Hoppang Character" />
                                 </div>
                                 <div className="app-info-text">
-                                    <h4>호빵 v3.3.3</h4>
+                                    <h4>호빵 v3.3.4</h4>
                                     <p>더 나은 서비스를 위해 지속적으로 개선하고 있습니다</p>
                                 </div>
                             </div>
